262dc9c09a0c4578818afc42fa43a5710d35b639
jose.filho <jose.filho@intermaritima.com.br>
2025-10-07 23:33:08 -0300

    docker compose

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import react from '@vitejs/plugin-react';
-import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
@@ -8,9 +8,15 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
+            buildDirectory: 'build', // 👈 força saída em public/build
         }),
         react(),
     ],
+    build: {
+        outDir: 'public/build', // 👈 garante compatibilidade com Laravel em produção
+        manifest: true,
+        emptyOutDir: true,
+    },
     esbuild: {
         jsx: 'automatic',
     },
