df8abd94e39191225707e851c5dea253e00e2cd4
Paulo Gabriel <paulogabriellcruz@gmail.com>
2025-11-13 23:08:05 -0300

    Fx

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -45,7 +45,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended('/pdv');
+        return redirect()->to(route('pdv', absolute: false));
     }
 
     /**
